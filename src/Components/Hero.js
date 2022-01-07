5be9358deee646fcbc768e81d45af8ea56c60784
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-01-07 17:54:57 +0600

    Hero Button added

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Box, Container, Grid, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import banner_img from '../images/hero-image.png';
 const Hero = () => {
     return (
@@ -15,9 +17,11 @@ const Hero = () => {
                         <Typography sx={{ color: 'rgba(255, 255, 255, 0.7)', fontSize: '13.9375px', width: '355.74px', height: '78px', lineHeight: '26px' }}>
                             Enver studio is a digital studio that offers several services such as UI/UX Design to developers, we will provide the best service for those of you who use our services.
                         </Typography>
+                        <Button variant="contained" sx={{ width: '135.39px', height: '38.49px', marginTop: '32.84px', backgroundColor: '#5454D4', borderRadius: '2.65476px' }}>Contained <ArrowRightAltIcon sx={{ transform: 'rotate(-50deg)' }} /></Button>
+
                     </Grid>
                     <Grid item lg={5} md={6} sm={12} xs={12} >
-                        <Box sx={{ width: '100%' }}>
+                        <Box style={{ width: '100%' }}>
                             <img src={banner_img} alt="" />
                         </Box>
                     </Grid>
