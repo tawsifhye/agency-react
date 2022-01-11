44a150b46bf05a872f55db85a3ebb2b63bf2b307
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-01-11 14:19:33 +0600

    Contaqct icon added

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -3,6 +3,44 @@ import { Box } from '@mui/system';
 import React from 'react';
 
 const ContactUs = () => {
+    const styles = {
+        circleIcon: {
+            position: 'absolute',
+            width: '36.55px',
+            height: '36.55px',
+            left: '337.53px',
+            bottom: 0,
+            border: '1.99107px solid #FFFFFF',
+            borderRadius: '50%',
+            '&::after': {
+                content: '""',
+                position: 'absolute',
+                width: '36.55px',
+                height: '36.55px',
+                left: ' 11px',
+                top: '8.39px',
+                border: '0.66369px solid #FFFFFF',
+                borderRadius: '50%',
+            }
+        },
+        plusIcon: {
+            background: '#E7E7E7',
+            height: '26px',
+            position: 'absolute',
+            top: "16.67%",
+            right: "16.67%",
+            width: '5px',
+            '&::after': {
+                background: '#E7E7E7',
+                content: '""',
+                height: '5px',
+                position: 'absolute',
+                left: '-10px',
+                top: '10px',
+                width: '26px'
+            }
+        },
+    }
     return (
         <>
             <Box spacing={2}
@@ -14,7 +52,6 @@ const ContactUs = () => {
                         md: 'row',
                         sm: 'column',
                         xs: 'column'
-
                     },
                     px: {
                         md: 0,
@@ -22,8 +59,11 @@ const ContactUs = () => {
                         xs: '20px'
                     },
                     justifyContent: 'space-around',
-                    py: '100px'
+                    py: '100px',
+                    position: 'relative'
                 }}>
+                <Box sx={styles.circleIcon} />
+                <Box sx={styles.plusIcon} />
                 <Box >
                     <Typography
                         sx={{
