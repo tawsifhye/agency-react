7426bd30afa1783410d8835fc97f2421ca3d676a
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-01-11 12:35:15 +0600

    Portfolio icons added

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,21 +1,103 @@
 import { Container, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import React from 'react';
 import portfolio_image1 from '../images/portfolio-img1.png'
 import portfolio_image2 from '../images/portfolio-img2.png'
 import portfolio_image3 from '../images/portfolio-img3.png'
-const Styles = {
+const styles = {
     title: {
         fontWeight: 800,
         fontSize: '38.494px',
         lineHeight: '53px',
         color: '#FFFFFF',
     },
-}
 
+    circleIcon: {
+        position: 'absolute',
+        width: '36.55px',
+        height: '36.55px',
+        right: '31px',
+        top: '31px',
+        border: '1.99107px solid #FFFFFF',
+        borderRadius: '50%',
+        '&::before': {
+            content: '""',
+            position: 'absolute',
+            width: '36.55px',
+            height: '36.55px',
+            left: '11px',
+            top: '8.39px',
+            border: '0.66369px solid #FFFFFF',
+            borderRadius: '50%'
+        },
+    },
+    zigzagIcon: {
+        position: 'absolute',
+        width: ' 18.58px',
+        height: '18.58px',
+        left: '60px',
+        bottom: '-40px',
+        borderBottom: '2.65476px solid #E7E7E7',
+        borderRight: '2.65476px solid #E7E7E7',
+        transform: 'rotate(52deg)',
+        '&::before': {
+            content: '""',
+            position: 'absolute',
+            width: '18.58px',
+            height: '18.58px',
+            left: '-16px',
+            top: '21.31px',
+            borderBottom: '2.65476px solid #E7E7E7',
+            borderRight: '2.65476px solid #E7E7E7',
+            transform: 'rotate(347deg)'
+        },
+        '&::after': {
+            content: '""',
+            position: 'absolute',
+            width: '18.58px',
+            height: '18.58px',
+            left: '18px',
+            top: '-20.69px',
+            borderBottom: '2.65476px solid #E7E7E7',
+            transform: 'rotate(347deg)'
+        }
+    },
+    slider: {
+        background: '#5454D4',
+        height: 0,
+        width: '13px',
+        padding: '4px',
+        position: 'absolute',
+        left: '50%',
+        borderRadius: '10px',
+        '&::before': {
+            content: '""',
+            height: '5px',
+            width: '5px',
+            background: '#121213',
+            position: 'absolute',
+            right: '-20px',
+            top: '2px',
+            borderRadius: '5px',
+        },
+        '&::after': {
+            content: '""',
+            height: '5px',
+            width: '5px',
+            background: '#121213',
+            position: 'absolute',
+            right: '-11px',
+            top: '2px',
+            borderRadius: '5px'
+        }
+    }
+}
 const Portfolio = () => {
     return (
-        <Container maxWidth='lg' sx={{ marginTop: '131px' }}>
-            <Typography sx={Styles.title}>
+        <Container maxWidth='lg' sx={{ marginTop: '131px', position: 'relative' }}>
+            <Box sx={styles.circleIcon} />
+            <Box sx={styles.zigzagIcon} />
+            <Typography sx={styles.title}>
                 Our Awesome Portofolio
             </Typography>
             <Grid container spacing={2} sx={{
@@ -49,6 +131,7 @@ const Portfolio = () => {
                     }}
                 >
                     <img style={{ width: '75%' }} src={portfolio_image3} alt="" />
+                    <Box sx={styles.slider} />
                 </Grid>
 
             </Grid>
