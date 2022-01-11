91bdb0f08b7812793faf842b20c057af36e2b8e2
Staff Asia <mahbuburrahman@Ismail-Hossain.local>
2022-01-11 10:47:26 +0600

    hero icon added

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -40,10 +40,154 @@ const Hero = () => {
                 zIndex: -1,
             }
 
-        }
+        },
 
+        plusIcon: {
+            background: '#E7E7E7',
+            height: '26px',
+            position: 'absolute',
+            left: '372px',
+            top: '390px',
+            width: '5px',
+            '&::after': {
+                background: '#E7E7E7',
+                content: '""',
+                height: '5px',
+                position: 'absolute',
+                left: '-10px',
+                top: '10px',
+                width: '26px'
+            }
+        },
+
+        circleIcon: {
+            position: 'absolute',
+            width: '36.55px',
+            height: '36.55px',
+            left: '349px',
+            top: '3.12px',
+            border: '1.99107px solid #FFFFFF',
+            borderRadius: '50%',
+            '&::before': {
+                content: '""',
+                position: 'absolute',
+                width: '36.55px',
+                height: '36.55px',
+                left: '11px',
+                top: '8.39px',
+                border: '0.66369px solid #FFFFFF',
+                borderRadius: '50%'
+            }
+
+        },
+
+        zigzagIcon: {
+            position: 'absolute',
+            width: ' 18.58px',
+            height: '18.58px',
+            left: {
+                lg: '-200.56px',
+                sm: '16px',
+                xs: '30px',
+            },
+            top: '3.31px',
+            borderBottom: '2.65476px solid #E7E7E7',
+            borderRight: '2.65476px solid #E7E7E7',
+            transform: 'rotate(52deg)',
+            '&::before': {
+                content: '""',
+                position: 'absolute',
+                width: '18.58px',
+                height: '18.58px',
+                left: '-16px',
+                top: '21.31px',
+                borderBottom: '2.65476px solid #E7E7E7',
+                borderRight: '2.65476px solid #E7E7E7',
+                transform: 'rotate(347deg)'
+            },
+            '&::after': {
+                content: '""',
+                position: 'absolute',
+                width: '18.58px',
+                height: '18.58px',
+                left: '18px',
+                top: '-20.69px',
+                borderBottom: '2.65476px solid #E7E7E7',
+                transform: 'rotate(347deg)'
+            }
+
+        },
+
+        triangleIcon1: {
+            position: 'absolute',
+            width: ' 47.45px',
+            height: ' 48.45px',
+            left: {
+                lg: '-200.56px',
+                sm: '16px',
+                xs: '16px',
+            },
+            top: '334px',
+            borderBottom: '2.65476px solid #E7E7E7',
+            transform: 'rotate(387deg)',
+            '&:: before': {
+                content: '""',
+                position: 'absolute',
+                width: '51.45px',
+                height: '50.45px',
+                left: '-14px',
+                top: '32px',
+                borderBottom: '2.65476px solid #E7E7E7',
+                transform: 'rotate(300deg)',
+            },
+            '&:: after': {
+                content: '""',
+                position: 'absolute',
+                width: ' 49.45px',
+                height: ' 48.45px',
+                left: '8px',
+                top: '35px',
+                borderBottom: '2.65476px solid #E7E7E7',
+                transform: 'rotate(424deg)'
+
+            }
+        },
+
+        triangleIcon2: {
+            position: 'absolute',
+            width: '47.45px',
+            height: '48.45px',
+            left: {
+                lg: '-195.56px',
+                sm: '19px',
+                xs: '19px',
+            },
+            top: '353px',
+            borderBottom: '1.65476px solid #E7E7E7',
+            transform: 'rotate(387deg)',
+            '&:: before': {
+                content: '""',
+                position: 'absolute',
+                width: '48.45px',
+                height: '48.45px',
+                left: '-11px',
+                top: '33px',
+                borderBottom: '1.65476px solid #E7E7E7',
+                transform: 'rotate(300deg)',
+            },
+            '&::after': {
+                content: '""',
+                position: 'absolute',
+                width: '49.45px',
+                height: '48.45px',
+                left: '8px',
+                top: '35px',
+                borderBottom: '1.65476px solid #E7E7E7',
+                transform: 'rotate(424deg)'
+
+            }
+        }
     }
-    console.log();
     return (
 
         <Container maxWidth='lg' sx={{ marginTop: '103px' }}>
@@ -87,9 +231,15 @@ const Hero = () => {
                 <Grid item lg={5} md={6} sm={12} xs={12} >
                     <Box sx={Styles.imageBox}
                     >
+                        <Box sx={Styles.plusIcon}></Box>
+                        <Box sx={Styles.circleIcon}></Box>
+                        <Box sx={Styles.zigzagIcon}></Box>
+                        <Box sx={Styles.triangleIcon1}></Box>
+                        <Box sx={Styles.triangleIcon2}></Box>
                         <img
                             style={Styles.image}
                             src={banner_img} alt="" />
+
                     </Box>
                 </Grid>
             </Grid>
